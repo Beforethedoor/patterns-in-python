51a7fcf80cbf8288d5bc79c48929913ccbdfd3b9
Sergei Iniakin <beforethedoor@Sergeis-MacBook-Pro.local>
2022-05-09 11:32:11 +0300

    add observer pattern

---
+++ observer.py
@@ -0,0 +1,36 @@
+class Subscriber:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def update(self, message: str) -> None:
+        print(f'{self.name} got message: "{message}"')
+
+
+class Publisher:
+    def __init__(self) -> None:
+        self.subscribers = set()
+
+    def register(self, who: Subscriber) -> None:
+        self.subscribers.add(who)
+
+    def unregister(self, who: Subscriber) -> None:
+        self.subscribers.discard(who)
+
+    def dispatch(self, message: str) -> None:
+        for subscriber in self.subscribers:
+            subscriber.update(message)
+
+pub = Publisher()
+
+bob = Subscriber(name="Bob")
+alice = Subscriber(name="Alice")
+john = Subscriber(name="John")
+
+pub.register(bob)
+pub.register(alice)
+pub.register(john)
+
+pub.dispatch(message="It's lunchtime!")
+
+pub.unregister(bob)
+pub.dispatch(message="Time for dinner!")

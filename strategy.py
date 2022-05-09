19fe15e8c3c21674816e8501a30dac65b9ec8257
Sergei Iniakin <beforethedoor@Sergeis-MacBook-Pro.local>
2022-05-09 11:10:04 +0300

    first commit

---
+++ strategy.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+
+class Bike(ABC):
+
+    @abstractmethod
+    def run_bike(self) -> None:
+        ...
+
+
+class SportBike(Bike):
+
+    def run_bike(self) -> None:
+        print("Run sport bike!")
+
+
+class CasualBike(Bike):
+
+    def run_bike(self) -> None:
+        print("Run casual bike!")
+
+
+class Human():
+
+    def __init__(self, bike: Bike) -> None:
+        self.bike = bike
+
+    def run(self) -> None:
+        self.bike.run_bike()
+
+
+sport_bike = SportBike()
+casual_bike = CasualBike()
+
+h = Human(bike=sport_bike)
+h.run()
+
+h.bike = casual_bike
+h.run()

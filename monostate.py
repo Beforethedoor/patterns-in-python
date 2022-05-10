2276df18915da178a0d367f7ee07486efcc173d7
Sergei Iniakin <inyakin97@gmail.com>
2022-05-10 11:35:35 +0300

    add monostate pattern

---
+++ monostate.py
@@ -0,0 +1,21 @@
+class ThreadData:
+    __shared_attrs = {
+        'name': 'thread_1',
+        'data': {},
+        'id': 1,
+    }
+
+    def __init__(self) -> None:
+        self.__dict__ = self.__shared_attrs
+
+
+obj1 = ThreadData()
+obj2 = ThreadData()
+
+obj1.id = 3
+print(obj1.__dict__)
+print(obj2.__dict__)
+
+obj1.attr_new = 'new attr'
+print(obj1.__dict__)
+print(obj2.__dict__)
